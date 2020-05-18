acfcd73e199f50033271e88d9ab3a9e48010757c
Yitao Chen <ychen404@asu.edu>
2020-05-18 11:41:57 -0700

    load weights

--- src/network.c
+++ src/network.c
@@ -55,6 +55,7 @@ network *load_network(char *cfg, char *weights, int clear)
     network *net = parse_network_cfg(cfg);
     if(weights && weights[0] != 0){
         load_weights(net, weights);
+        print("*load_network\n")
     }
     if(clear) (*net->seen) = 0;
     return net;
