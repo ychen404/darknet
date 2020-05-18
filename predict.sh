27a656c36144cbc94f161adb4af618485e39e7a8
Ubuntu <ubuntu@ip-172-31-93-145.ec2.internal>
2020-05-18 18:25:42 +0000

    first commit

---
+++ predict.sh
@@ -0,0 +1,1 @@
+./darknet classifier predict cfg/cifar.data cfg/cifar_small.cfg backup/cifar_small.weights data/dog.jpg
---
+++ train.sh
@@ -0,0 +1,1 @@
+./darknet classifier train cfg/cifar.data cfg/cifar_small.cfg
