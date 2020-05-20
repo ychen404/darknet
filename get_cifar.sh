cc065e69024d39ac0b877c872948ccc7b67de7f0
Yitao Chen <ychen404@asu.edu>
2020-05-20 09:43:42 -0700

    update

--- get_cifar.sh
+++ get_cifar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd data;
 wget https://pjreddie.com/media/files/cifar.tgz --no-check-certificate \
-&& tar xzf cifar.tgz ;
+&& tar xzf cifar.tgz;
 cd cifar;
 find `pwd`/train -name \*.png > train.list;
 find `pwd`/test -name \*.png > test.list;
